c52cd47073882a24083a2e7de89a273db6b584c8
Nico Bazzoni <you@example.com>
2021-02-11 14:14:49 -0500

    commit tree b6464bfaf42ec9840217d4b00eb8fc7506b8e6a6

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(values)
+    @name = values[:name]
+    @type = values[:type]
+    @booty = values[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
 end
